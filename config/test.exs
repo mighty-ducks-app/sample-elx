d86f364c387a4092537eb12ca7eb2d0d75d1d342
David Rouchy <david@rouchy.dev>
2025-08-09 14:30:24 +0100

    Basic Phoenix app

---
+++ config/test.exs
@@ -0,0 +1,24 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :sample, SampleWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "ZuwTwx0tnef+xc0ZtyVEUUHd6nC5aeeE8BaiJlwrqY/zUlBMXDNpnbxU5lSDTtIy",
+  server: false
+
+# In test we don't send emails
+config :sample, Sample.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
